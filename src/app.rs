e479da5a74d4f26117c03ff8f230d60932acb78b
dziedziak <jas.dzied@gmail.com>
2022-05-19 21:46:46 +0100

    added bunch of stuff, picking usernames, dark mode

--- src/app.rs
+++ src/app.rs
@@ -6,15 +6,19 @@ use std::thread;
 use std::time::Duration;
 use std::sync::{Arc, RwLock};
 use rand::{Rng, thread_rng};
+use egui::RichText;
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct ChatApp {
     message: String,
+    username: String,
     messages: Arc<RwLock<Vec<String>>>,
 
     #[serde(skip)]
-    socket: WebSocket<MaybeTlsStream<TcpStream>>
+    socket: WebSocket<MaybeTlsStream<TcpStream>>,
+    #[serde(skip)]
+    mode: i32
 }
 
 impl Default for ChatApp {
@@ -24,7 +28,7 @@ impl Default for ChatApp {
             let mut rng = thread_rng();
             let mut temp: i64 = 0;
             for i in 1..10 {
-                temp += rng.gen_range(0..10)*10i64.pow(i);
+                temp += rng.gen_range(1..10)*10i64.pow(i);
             }
             temp
         };
@@ -51,17 +55,50 @@ impl Default for ChatApp {
 
         Self {
             message: "".to_owned(),
+            username: "".to_owned(),
             messages: Arc::clone(&messages),
-            socket: writer_socket
+            socket: writer_socket,
+            mode: 0
         }
 
     }
 }
 
 impl ChatApp {
-    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+
+        cc.egui_ctx.set_visuals(egui::Visuals::dark());
+
+        let mut fonts = egui::FontDefinitions::default();
+
+        fonts.font_data.insert("hack".to_owned(),
+            egui::FontData::from_static(include_bytes!("../Hack Regular Nerd Font Complete Mono.ttf")));
+
+        fonts.families.get_mut(&egui::FontFamily::Monospace).unwrap()
+            .insert(0, "hack".to_owned());
+
+        cc.egui_ctx.set_fonts(fonts);
         Default::default()
     }
+}
+
+macro_rules! hack_text {
+    ( $template:expr, $( $x:expr ),* ) => {
+        RichText::new(format!(
+            $template, $($x),*
+        )).font(egui::FontId::monospace(15.0))
+    };
+    ( $template:expr ) => {
+        RichText::new($template).font(egui::FontId::monospace(15.0))
+    }
+}
+
+fn multiply(string: &str, count: usize) -> String {
+    let mut result = String::new();
+    for _ in 0..count {
+        result.push_str(string);
+    }
+    result
 }
 
 impl eframe::App for ChatApp {
@@ -72,34 +109,89 @@ impl eframe::App for ChatApp {
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
 
-        let Self { message, messages, socket } = self;
+        let Self { message, username, messages, socket, mode } = self;
+
+        match mode {
+            0 => {
 
         egui::CentralPanel::default().show(&ctx, |ui| {
-            match messages.try_read() {
-                Ok(lock) => {
-                    for message in lock.iter() {
-                        ui.label(message);
-                    }
-                },
-                Err(_) => {
-                    ui.label("no lock?");
-                }
+            ui.label("enter username: ");
+            ui.text_edit_singleline(username);
+            if ui.button("Join").clicked() {
+                socket.write_message(Message::Text(username.to_string())).unwrap();
+                *mode += 1;
             }
         });
+
+            },
+            1 => {
 
         egui::TopBottomPanel::bottom("input_box")
             .min_height(45.0)
             .show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.label("enter message: ");
-                ui.text_edit_singleline(message);
+                ui.text_edit_multiline(message);
                 let button = egui::Button::new("Send");
-                if ui.add_sized([45.0, 45.0], button).clicked() {
-                    socket.write_message(Message::Text(message.to_string())).unwrap();
+                if
+                    ui.add_sized([45.0, 45.0], button).clicked() ||
+                    (
+                        ctx.input().keys_down.contains(&egui::Key::Enter) &&
+                        !ctx.input().modifiers.matches(egui::Modifiers::SHIFT) &&
+                        (*message).trim() != ""
+                    )
+                {
+                    socket.write_message(Message::Text(message.trim().to_string())).unwrap();
                     message.clear();
                 }
             })
         });
+
+        egui::CentralPanel::default().show(&ctx, |ui| {
+            egui::ScrollArea::vertical()
+                .auto_shrink([false; 2])
+                .show(ui, |ui| {
+
+            match messages.try_read() {
+                Ok(lock) => {
+
+
+                    for message in lock.iter() {
+
+                        let parts: Vec<&str> = message.split('|').collect();
+                        let sender_str = base64::decode(parts[0]).unwrap();
+                        let sender = String::from_utf8(sender_str).unwrap();
+                        let message_str = base64::decode(parts[1]).unwrap();
+                        let message = String::from_utf8(message_str).unwrap();
+
+                        let mut longest = sender.len();
+                        for line in message.split('\n') {
+                            if line.len() > longest {
+                                longest = line.len();
+                            }
+                        }
+
+                        ui.label(hack_text!("╭─{}{}─╮", sender, "─".repeat(longest-sender.len())));
+                        for line in message.split('\n') {
+                            ui.label(hack_text!("│ {:longest$} │", line));
+                        }
+                        ui.label(hack_text!("╰{}╯", "─".repeat(longest+2)));
+
+                    }
+                },
+                Err(_) => {
+                    ui.label("no lock?");
+                }
+            }
+
+            });
+        });
+
+            },
+            _ => {}
+        }
+
+
 
     }
 }
