0fa9e98928087937bc963d839e90c2a9cbe256ef
dziedziak <jas.dzied@gmail.com>
2022-05-19 18:18:05 +0100

    added chat

--- src/app.rs
+++ src/app.rs
@@ -1,111 +1,105 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use tungstenite::{connect, Message, WebSocket};
+use tungstenite::stream::MaybeTlsStream;
+use url::Url;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+use std::sync::{Arc, RwLock};
+use rand::{Rng, thread_rng};
 
-    // this how you opt-out of serialization of a member
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+pub struct ChatApp {
+    message: String,
+    messages: Arc<RwLock<Vec<String>>>,
+
     #[serde(skip)]
-    value: f32,
+    socket: WebSocket<MaybeTlsStream<TcpStream>>
 }
 
-impl Default for TemplateApp {
+impl Default for ChatApp {
     fn default() -> Self {
+
+        let uid = {
+            let mut rng = thread_rng();
+            let mut temp: i64 = 0;
+            for i in 1..10 {
+                temp += rng.gen_range(0..10)*10i64.pow(i);
+            }
+            temp
+        };
+        println!("{}", uid);
+
+        let (mut reader_socket, _) = connect(Url::parse("ws://127.0.0.1:8080").unwrap()).expect("Can't connect");
+        let (mut writer_socket, _) = connect(Url::parse("ws://127.0.0.1:8080").unwrap()).expect("Can't connect");
+
+        reader_socket.write_message(Message::Text(format!("{}r", uid))).unwrap();
+        writer_socket.write_message(Message::Text(format!("{}w", uid))).unwrap();
+
+        let messages = Arc::new(RwLock::new(vec![]));
+        let link = Arc::clone(&messages);
+
+        thread::spawn(move || {
+            loop {
+                let msg = reader_socket.read_message().expect("Error reading message").into_text().unwrap();
+                let mut message_list = link.write().unwrap();
+                message_list.push(msg);
+                drop(message_list);
+                thread::sleep(Duration::from_millis(1000));
+            }
+        });
+
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            message: "".to_owned(),
+            messages: Arc::clone(&messages),
+            socket: writer_socket
         }
+
     }
 }
 
-impl TemplateApp {
-    /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customized the look at feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
-
+impl ChatApp {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Default::default()
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for ChatApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
 
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+        let Self { message, messages, socket } = self;
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
+        egui::CentralPanel::default().show(&ctx, |ui| {
+            match messages.try_read() {
+                Ok(lock) => {
+                    for message in lock.iter() {
+                        ui.label(message);
                     }
-                });
-            });
-        });
-
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
+                },
+                Err(_) => {
+                    ui.label("no lock?");
+                }
             }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
         });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+        egui::TopBottomPanel::bottom("input_box")
+            .min_height(45.0)
+            .show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.label("enter message: ");
+                ui.text_edit_singleline(message);
+                let button = egui::Button::new("Send");
+                if ui.add_sized([45.0, 45.0], button).clicked() {
+                    socket.write_message(Message::Text(message.to_string())).unwrap();
+                    message.clear();
+                }
+            })
         });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
     }
 }
